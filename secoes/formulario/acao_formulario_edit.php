f1a280bd012d5313837c1735a61f8eb2e41c9cff
Onildo <onildojoaocosta@gmail.com>
2018-02-17 01:29:36 -0300

    Third php fix - sql and edit screen

--- secoes/formulario/acao_formulario_edit.php
+++ secoes/formulario/acao_formulario_edit.php
@@ -663,33 +663,6 @@ if ($_POST != null)
     // COMANDO SQL CADASTRO CEB
     $sql_cad = "INSERT INTO cad_ceb
                 (cad_idPoste, cad_data_cadastro, cad_cpf_usuario, cad_nome_usuario,
-                cad_dg_cg_p2_lat, cad_dg_cg_p2_lng, cad_dg_cg_p3_lat, cad_dg_cg_p3_lng, 
-                cad_dg_cg_p4_lat, cad_dg_cg_p4_lng, cad_dg_cg_p5_lat, cad_dg_cg_p5_lng, 
-                cad_dg_cg_p6_lat, cad_dg_cg_p6_lng,
-                cad_dt_potencia_lampada, cad_dt_tipo_lampada, cad_dt_tipo_luminaria,
-                cad_dt_tipo_braco, cad_dt_altura_poste, cad_dt_material_poste, cad_dt_uso_poste,
-                cad_dt_tipo_comando, cad_dt_tipo_reator, cad_dt_forma_distribuicao_energia,
-                cad_df_aparencia_ponto_luminoso, cad_df_observacao_ponto_luminoso,
-                cad_df_arvore_ofuscando_iluminacao, cad_df_situacao_poste,
-                cad_df_proximidade_luminaria_rede_energia, cad_df_local_dificil_acesso,
-                cad_md_posicao_poste, 
-                cad_md_lampada_apagada_pl1, cad_md_lampada_apagada_pl2, cad_md_lampada_apagada_pl3,
-                cad_md_lampada_apagada_pl4, cad_md_lampada_apagada_pl5, cad_md_lampada_apagada_pl6,
-                cad_md_c1_l1, cad_md_c2_l1, cad_md_c3_l1,
-                cad_md_c4_l1, cad_md_c5_l1, cad_md_c6_l1,
-                cad_md_c7_l1, cad_md_c8_l1, cad_md_c9_l1,
-                cad_md_c1_l2, cad_md_c2_l2, cad_md_c3_l2,
-                cad_md_c4_l2, cad_md_c5_l2, cad_md_c6_l2,
-                cad_md_c7_l2, cad_md_c8_l2, cad_md_c9_l2,
-                cad_md_c1_l3, cad_md_c2_l3, cad_md_c3_l3,
-                cad_md_c4_l3, cad_md_c5_l3, cad_md_c6_l3,
-                cad_md_c7_l3, cad_md_c8_l3, cad_md_c9_l3,
-                cad_md_c1_l4, cad_md_c2_l4, cad_md_c3_l4,
-                cad_md_c4_l4, cad_md_c5_l4, cad_md_c6_l4,
-                cad_md_c7_l4, cad_md_c8_l4, cad_md_c9_l4,
-                cad_md_c1_l5, cad_md_c2_l5, cad_md_c3_l5,
-                cad_md_c4_l5, cad_md_c5_l5, cad_md_c6_l5,
-                cad_md_c7_l5, cad_md_c8_l5, cad_md_c9_l5,
                 cad_md_distancia_postes_p1_p3, cad_md_distancia_postes_p3_p5,
                 cad_md_distancia_postes_p2_p4, cad_md_distancia_postes_p4_p6,
                 cad_md_distancia_postes_p1_p2,
@@ -698,33 +671,6 @@ if ($_POST != null)
                 cad_md_largura_via_l1) 
                 VALUES 
                 ('$id_poste', '$data_cadastro', '$usu_cpf', '$usu_nome',
-                '$dg_cg_p2_lat', '$dg_cg_p2_lng', '$dg_cg_p3_lat', '$dg_cg_p3_lng',
-                '$dg_cg_p4_lat', '$dg_cg_p4_lng', '$dg_cg_p5_lat', '$dg_cg_p5_lng',
-                '$dg_cg_p6_lat', '$dg_cg_p6_lng',
-                '$dt_potencia_lampada', '$dt_tipo_lampada', '$dt_tipo_luminaria',
-                '$dt_tipo_braco', '$dt_altura_poste', '$dt_material_poste', '$dt_uso_poste',
-                '$dt_tipo_comando', '$dt_tipo_reator', '$dt_forma_distribuicao_energia',
-                '$df_aparencia_ponto_luminoso', $df_observacao_ponto_luminoso,
-                '$df_arvore_ofuscando_iluminacao','$df_situacao_poste', 
-                '$df_proximidade_luminaria_rede_energia','$df_local_dificil_acesso',
-                '$md_posicao_poste',
-                '$md_lampada_apagada_pl1', '$md_lampada_apagada_pl2', '$md_lampada_apagada_pl3',
-                '$md_lampada_apagada_pl2', '$md_lampada_apagada_pl5', '$md_lampada_apagada_pl6',
-                '$md_c1_l1', '$md_c2_l1', '$md_c3_l1',
-                '$md_c4_l1', '$md_c5_l1', '$md_c6_l1',
-                '$md_c7_l1', '$md_c8_l1', '$md_c9_l1',
-                '$md_c1_l2', '$md_c2_l2', '$md_c3_l2',
-                '$md_c4_l2', '$md_c5_l2', '$md_c6_l2', 
-                '$md_c7_l2', '$md_c8_l2', '$md_c9_l2',
-                '$md_c1_l3', '$md_c2_l3', '$md_c3_l3',
-                '$md_c4_l3', '$md_c5_l3', '$md_c6_l3',
-                '$md_c4_l3', '$md_c5_l3', '$md_c6_l3',
-                '$md_c1_l4', '$md_c2_l4', '$md_c3_l4',
-                '$md_c4_l4', '$md_c5_l4', '$md_c6_l4',
-                '$md_c7_l4', '$md_c8_l4', '$md_c9_l4',
-                '$md_c1_l5', '$md_c2_l5', '$md_c3_l5',
-                '$md_c4_l5', '$md_c5_l5', '$md_c6_l5',
-                '$md_c7_l5', '$md_c8_l5', '$md_c9_l5',
                 '$md_distancia_postes_p1_p3', '$md_distancia_postes_p3_p5', 
                 '$md_distancia_postes_p2_p4', '$md_distancia_postes_p4_p6',
                 '$md_distancia_postes_p1_p2', 
@@ -745,19 +691,16 @@ if ($_POST != null)
     }
 
     // COMANDO SQL POSTE PRINCIPAL
-    $sql_pp = "INSERT INTO pp_posteprincipal
-                (pp_id,
-                pp_Estado, pp_Cidade,
-                pp_Endereco, pp_Endereco_Observacao,
-                pp_Regiao_Administrativa,
-                pp_LAT_DD, pp_LNG_DD, pp_Classe_Via
-                ) VALUES 
-                ('$id_poste',
-                '$pp_estado', '$pp_cidade',
-                '$pp_endereco', '$pp_endereco_observacao',
-                '$pp_regiao_administrativa',
-                '$dg_cg_p1_lat', '$dg_cg_p1_lng',
-                '$dg_classe_via')";
+    $sql_pp = "UPDATE pp_posteprincipal
+               SET pp_Estado = '$pp_estado',
+               pp_Cidade = '$pp_cidade',
+               pp_Endereco = '$pp_endereco',
+               pp_Endereco_Observacao = '$pp_endereco_observacao',
+               pp_Regiao_Administrativa = '$pp_regiao_administrativa',
+               pp_LAT_DD = '$dg_cg_p1_lat',
+               pp_LNG_DD = '$dg_cg_p1_lng',
+               pp_classe_via = '$pp_classe_via'
+               WHERE pp_id = '$id_poste'";
 
     $retorno_pp = $conexao->query($sql_pp);
 
